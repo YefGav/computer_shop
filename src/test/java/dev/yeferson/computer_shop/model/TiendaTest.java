68a3e02a4d7daaf90eecf70264b8228a2dfe34d0
unknown <gaviriayeferson2@gmail.com>
2025-02-23 14:51:38 +0100

    Add test for Tienda constructor and getters using BeforeEach

---
+++ src/test/java/dev/yeferson/computer_shop/model/TiendaTest.java
@@ -0,0 +1,29 @@
+package dev.yeferson.computer_shop.model;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TiendaTest {
+
+    private Tienda tienda;
+
+    @BeforeEach
+
+    void setUp() {
+        tienda = new Tienda ("Tech Store", "Yeferson Gaviria", "12345-67890");
+    }
+
+    @Test
+    @DisplayName ("Test if the store is created")
+
+    void testConstructorYGetters() {
+
+        assertThat(tienda.getNombre(), is("Tech Store"));
+        assertThat(tienda.getPropietario(), is("Yeferson Gaviria"));
+        assertThat(tienda.getIdentificacionTributaria(), is("12345-67890"));
+    }
+}
