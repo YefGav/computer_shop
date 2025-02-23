9c50a3b12401f34532960dd895d9e232adc1c0ed
unknown <gaviriayeferson2@gmail.com>
2025-02-23 17:03:21 +0100

    Implement Controller constructor

---
+++ src/main/java/dev/yeferson/computer_shop/Controller/Controller.java
@@ -0,0 +1,15 @@
+package dev.yeferson.computer_shop.Controller;
+
+import dev.yeferson.computer_shop.model.Tienda;
+import dev.yeferson.computer_shop.model.Computador;
+
+public class Controller {
+
+    private Tienda tienda;
+
+    public Controller(Tienda tienda) {
+        this.tienda = tienda;
+    }
+
+    
+}
